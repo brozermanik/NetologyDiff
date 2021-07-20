b682fca2bd88a51516924c651b4827ad3ef759e1
Bekhruz Gab <brozermanik@gmail.com>
2021-07-20 20:46:38 +0300

    Initial commit

---
+++ NetologyDiff/Class1.cs
@@ -0,0 +1,21 @@
+﻿namespace NetologyDiff
+{
+    public class Class1
+    {
+        public delegate string ExampleDelegate(int arg1, long arg2);
+
+        public event ExampleDelegate ExampleEventHandler;
+
+        public void CreateBinds()
+        {
+            ExampleEventHandler += (i, l) =>
+            {
+                if (i > l)
+                {
+                    return "Greater";
+                }
+                else return "Less";
+            };
+        }
+    }
+}
---
+++ NetologyDiff/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NetologyDiff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
